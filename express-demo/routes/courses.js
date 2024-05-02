9b33121657667b79abd27f3518bd9d692283fbce
Muhammad Ahsan <nawazehsen@gmail.com>
2024-05-02 04:44:37 -0700

    added put/delete functionality

--- express-demo/routes/courses.js
+++ express-demo/routes/courses.js
@@ -7,7 +7,6 @@ const router = express.Router()
 
 router.get('/',(req,res)=>{
     courseAPI.getCourses().then(courses=>{
-        
         res.send(courses)
     })
     
@@ -25,7 +24,7 @@ router.post('/',(req,res)=>{
     
     const schema = Joi.object({
         name:Joi.string().min(3).max(255).required(),
-        category:Joi.string().required(),
+        category:Joi.string().required().valid('web','mobile','network'),
         author:Joi.string(),
         tags:Joi.array().items(Joi.string()).min(1).required(),
         
@@ -37,9 +36,10 @@ router.post('/',(req,res)=>{
     
     if(error){
         const errors=[]
+
         for(err of error.details)
-            errors.push(err.message)
-        res.status(400).send(errors)
+            errors.push({name:err.context.key,message:err.message})
+        res.status(400).send({ errors })
         return;
     }
 
@@ -59,33 +59,63 @@ router.post('/',(req,res)=>{
 
 })
 router.put('/:id',(req,res)=>{
-    
-    let foundCourse = courses.find(course=>course.id===parseInt(req.params.id))
-    if(!foundCourse){
-        res.status(400).send(`Course ID doesn't exist`)
-        return;
-    }
 
     const schema = Joi.object({
-        name:Joi.string().min(3).required()
+        name:Joi.string().min(3).max(255),
+        category:Joi.string().valid('web','mobile','network'),
+        author:Joi.string(),
+        tags:Joi.array().items(Joi.string()).min(1),
+        isPublished:Joi.boolean(),
+        price:Joi.number().min(1).max(100)
     })
     const {value,error} = schema.validate(req.body)
     if(error){
-        res.status(400).send(error.details[0].message)
+        const errors=[]
+        for(err of error.details)
+            errors.push({name:err.context.key,message:err.message})
+        res.status(400).send({ errors })
         return;
     }
-    foundCourse.name = value.name
 
-    res.send(foundCourse)
+    courseAPI.updateCourse(req.params.id,value).then(result=>{
+        
+        if(result.message)
+            res.status(400).send({err_msg:`Course doesn't exist`})
+        res.send(result)
+        // if(!result){
+        //     res.status(400).send(`Course ID doesn't exist`)
+        //     return;
+        // }
+    })
+    // let foundCourse = courses.find(course=>course.id===parseInt(req.params.id))
+
+
+    // const schema = Joi.object({
+    //     name:Joi.string().min(3).required()
+    // })
+    // const {value,error} = schema.validate(req.body)
+    // if(error){
+    //     res.status(400).send(error.details[0].message)
+    //     return;
+    // }
+    // foundCourse.name = value.name
+
+    
 
 })
 router.delete('/:id',(req,res)=>{
-    let foundCourse = courses.find(course=>course.id===parseInt(req.params.id))
-    if(!foundCourse){
-        res.status(400).send(`Course ID doesn't exist`)
-        return;
-    }
-    courses.filter(course=>course.id!==foundCourse.id)
-    res.send(foundCourse)
+    courseAPI.deleteCourse(req.params.id).then(result=>{
+        console.log(result)
+        if(result.message)
+            res.status(400).send({err_msg:`Course doesn't exist`})
+        res.send(result)
+    })
+    // let foundCourse = courses.find(course=>course.id===parseInt(req.params.id))
+    // if(!foundCourse){
+    //     res.status(400).send(`Course ID doesn't exist`)
+    //     return;
+    // }
+    // courses.filter(course=>course.id!==foundCourse.id)
+    // res.send(foundCourse)
 })
 module.exports = router
